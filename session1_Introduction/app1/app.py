6c149b7e5e7b25439378750084568cc51318f9ac
Guhan Ganesan <bharadhiguhan@ymail.com>
2020-01-04 11:01:47 +0530

    Update app.py
    Url Building

--- session1_Introduction/app1/app.py
+++ session1_Introduction/app1/app.py
@@ -13,3 +13,26 @@ def index():
 	
 if __name__=="__main__":
   app.run(debug = True)
+
+"""
+
+
+@app.route('/')
+def home():
+    return "Welcome"
+
+@app.route('/hello')
+def hello():
+    return "hello"
+
+
+@app.route('/hello/<x>')
+def name(x):
+    return "hello Mr. %s" %x
+
+
+@app.route('/hello/<x>/<y>')
+def user(x,y):
+    return "Hello Mr. {} and your pass is {}".format(x,y)
+    
+ """
